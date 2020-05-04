ec415a6eb1df7402f55d8f70d42f9ad0bf3f52a1
Sneha Kanekar <skanekar@us.ibm.com>
2020-05-04 16:21:16 +0530

    Day 2 assignment added

---
+++ Day_2/main.tf
@@ -0,0 +1,16 @@
+variable "user" {
+  type = string
+}
+
+resource "random_password" "password" {
+  length           = 8
+  special          = true
+  override_special = "!#$%^*_"
+  min_upper        = 1
+  min_lower        = 1
+  min_special      = 1
+}
+
+output "user_msg" {
+  value = "Hi ${var.user}, your password is ${random_password.password.result}"
+}
