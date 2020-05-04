4bafe49640e84f81ef472a59a9952355b2ecac1c
Sneha Kanekar <skanekar@us.ibm.com>
2020-05-04 16:32:58 +0530

    Day 2 assignment added

--- Day_2/main.tf
+++ Day_2/main.tf
@@ -5,7 +5,7 @@ variable "user" {
 resource "random_password" "password" {
   length           = 8
   special          = true
-  override_special = "!#$%^*_"
+  override_special = "!#$%&*()-_=+[]{}<>:?"
   min_upper        = 1
   min_lower        = 1
   min_special      = 1
