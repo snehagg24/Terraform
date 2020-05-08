e580ea2de5e9e5d23ea86f897969ac96fa29adc9
Sneha Kanekar <skanekar@us.ibm.com>
2020-05-08 09:55:57 +0530

    Moved var to variables.tf

--- Day_3/provider.tf
+++ Day_3/provider.tf
@@ -1,5 +1,3 @@
-variable "ibmcloud_api_key" {}
-
 provider "ibm" {
 ibmcloud_api_key = var.ibmcloud_api_key
 generation = 2
--- Day_3/variables.tf
+++ Day_3/variables.tf
@@ -1,2 +1,4 @@
 variable "public_sshkey" {
 }
+variable "ibmcloud_api_key" {
+}
